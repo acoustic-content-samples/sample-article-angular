5084ed2eea94326bc5a24a23c81412dc13aebcec
Laura Pomerleau <llpomerl@us.ibm.com>
2016-12-12 12:22:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# sample-article-angular
+This sample web application illustrates how to call IBM Watson Content Hub APIs from client JavaScript. The application uses Angular, jQuery and Bootstrap to display an online application with navigation and cards with images from Watson Content Hub.
+
+This sample shows:
+* Authenticating to the Watson Content Hub and calling APIs that require authentication.
+* Using the taxonomies and categories APIs to retrieve the site's navigation.
+* Using the powerful search API to retrieve a list of content items for a specific content type.
+* Accessing content item elements for use in rendering.
+* Using an Image Profile with separate renditions for thumbnail and full sized views.
+* Dynamically building HTML for navigation, cards and details using Angular, jQuery and Bootstrap.
+* Navigate between different list and details views using Angular routing
+
+### About the Watson Content Hub authoring APIs
+
+The initial set of APIs provided by Watson Content Hub are for authoring services, which require authentication and are not optimized for retrieval by applications in production. Follow-on releases will provide "delivery" services which can be accessed anonymously and are optimized for caching and performance. However, the two APIs will be very similar, so to switch from authoring to delivery services you will just need to change the "authoring" portion of the service URL to "delivery". 
+
+### About authentication and tenant handling
+
+To call authenticated APIs, you need to first call the login service with the desired user name and password. This will return tenant information and an authentication token cookie for use on subsequent calls. The browser will include the authentication token cookie in subsequent requests. 
+
+The login call will return both a tenant ID and a tenant base URL in the response header. The tenant ID needs to be part of the URL for subsequent API calls. For example, the search service will have a URL like this:
+
+https://my.digitalexperience.ibm.com/api/[tenantID]/authoring/v1/search
+
+Typically you will use the base URL from the x-ibm-dx-tenant-base-url header to construct the service URL with the tenant ID. This is what's done in the sample code. An alternative approach is to obtain the tenant ID from the Watson Content Hub user interface. Click on the "i" information icon at the top left next to where it says "IBM Watson Content Hub" and you will see your tenant ID.
+
+Once you have the authentication token and have built the complete API link that includes the tenant ID, you can call any of the authenticated APIs by appending the specific API route for the service you want. This example calls the search service by appending "/authoring/v1/search" to the base URL, and the categories service via "/authoring/v1/categories".
+
+Note that every API service operation has specific user roles that it can be used with. In the API Explorer (see link below under [resources](#user-content-resources)) you can see the user roles for all operations.
+
+### Running the sample
+
+#### 1. Download the files
+
+Download the application files (html, js, and css) from the "public" folder into any folder on your workstation.
+
+#### 2. Update the user credentials
+
+This sample uses hard-coded user name and password set in the app.js file. Update the name and password values in that file.
+
+To avoid putting credentials in the source you could change the application to provide browser inputs for username and password. Also, as noted above, future Watson Content Hub releases will have support for "delivery" APIs that can be available without login.
+
+#### 3. Create all the resources needed to render the sample
+
+This application requires that you have an "Article" content type available and one or more content items using that type. Each content item will be assigned a category and the associated image will own a thumbnail rendition.
+
+##### 3a. Create an "Article" taxonomy with different categories
+
+In the WCH user interface, under Content Model -> Taxonomies, create a taxonomy named "Article" by clicking the "Create taxonomy" button. Add a few categories (eg: "Travel", "Lifestyle", "Fashion", "Tech", etc) by clicking the "Add parent category" button. Each category will be used as a link in the application's navgation.
+
+##### 3b. Create an "Article" Image profile with a "thumbnail" rendition
+
+In the WCH user interface, under Content Model -> Image profiles, create an image profile named "Article" by clicking the "Create image profile" button. Create a rendition with:
+- Label: thumbnail
+- Key: thumbnail (this should fill in automatically)
+- Width: 800
+- Height: 500
+Click the Add rendition button to complete the image profile. The thumbnail rendition will be used when rendering the site's card view.
+
+##### 3c. Create an "Article" content type
+
+In the WCH user interface, under Content Model -> Content types, create a content type named "Article" with the following elements. To set the "custom display" values, click on the little gear icon for the element.
+
+|Element name | Element type | Element custom display settings |
+| --- | --- | --- |
+|Title | Text | (none) |
+| Summary | Text | (none) |
+| Author | Text | (none) |
+| Body | Text | Multi line, Field width: 100, Field height: 10 |
+| Image | Image | Image profiles: Article |
+| Publish Date | Date (Field type "Single date") | (none) |
+| Category | Category | Select category: Article |
+
+##### 3d. Create some sample content items using the Article content type
+
+Go to Content -> My content and assets, and click the "Compose" link and select the "Article" content type. Fill in all the fields. Repeat to add various items to the different categories (to fill out the navigation).
+
+#### 4. Enable CORS support for your tenant
+
+Since this sample will run from a different domain than Watson Content Hub services, you will need to enable CORS (Cross-Origin Resource Sharing) for your tenant. You should only add domains that require access to the content and assets stored in your content hub, such as your web servers or your development environment. The domain format must be `protocol://server:port` where the protocol is either http or https, the server is either your server name or its IP address, and the port is the port number of your server. For example: `http://my.domain.org:80`. You can also use the `*` wildcard to enable CORS for any domain, though this isn't recommended from a security perspective. To control the CORS enablement for Watson Content Hub, go to Hub set up -> General settings -> Security tab. After adding your domain, be sure to click the Save button at the top right of the screen.
+
+#### 5. Load index.html in a browser
+
+You can do this right from the file system in Firefox, Chrome, or Safari browsers. Alternatively you can make the files available on any web server and open index.html in a browser using your web server URL.
+
+If all goes well you should see an application looking something like this (depending on what you entered for text and on the images you selected):
+
+![sample screenshot](/docs/article-sample-screenshot.jpg?raw=true "Sample screenshot")
+
+### Resources
+
+API Explorer reference documentation: https://developer.ibm.com/api/view/id-618
+
+Watson Content Hub developer center: https://developer.ibm.com/wch/
+
+Watson Content Hub forum: https://developer.ibm.com/answers/smartspace/wch/
---
+++ public/app.js
@@ -0,0 +1,311 @@
+/*
+ * Copyright 2016 IBM Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+*/
+
+
+// create the root app module
+const module = angular.module('SampleArticleMagazine', ['ui.router'])
+
+
+// Setup the angular routes for displaying article cards based on which tab is clicked
+module.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+	// default view
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+
+		// home page
+		.state('home', {
+			url: '/home',
+			template: '<article-cards></article-cards>'
+		})
+
+		// shows cards for the category name specified
+		.state('cards', {
+			url: '/:category',
+			template: '<article-cards></article-cards>'
+		})
+
+		// shows all the information for the content item specified in the item parameter
+		.state('cards.details', {
+			url: '/:itemName',
+			template: '<article-details></article-details>',
+			params: { item: null }
+		});
+}]);
+
+
+// always start on the home page
+module.run(['$state', function ($state) {
+	$state.transitionTo('home');
+}]);
+
+
+// Angular factory which accesses WCH
+module.factory('wchService', ['$http', function ($http) {
+
+	// Content Hub blueid username and password - replace these or add code to get these from inputs
+	const username = '[username]';
+	const password = '[email]';
+
+	// use this to get tenant from the basicauth call, filled in during login() below
+	let baseTenantUrl = '';
+	
+	/**
+	* Logs into Watson Content hub
+	* @return {Promise}
+	*/
+	function login() {
+		return $http({
+				method: 'GET',
+				url: 'https://my.digitalexperience.ibm.com/api/login/v1/basicauth',
+				headers: { 'Authorization': 'Basic ' + btoa(username + ':' + password) }
+			}).then(response => {
+				baseTenantUrl = response.headers('x-ibm-dx-tenant-base-url');
+				return response.data;
+			});
+	}
+	
+	/**
+	* Get the base url which includes the tenant id
+	* @return {String}
+	*/
+	function getBaseURL() {
+		return baseTenantUrl;
+	}
+	
+	/**
+	* Retrieves the ID for the taxonomy with a given name
+	* @param {String} taxonomyName: The string name of the taxonomy to fetch
+	* @return {Promise} resolves to a taxonomy Object
+	*/
+	function getTaxonomyByName(taxonomyName) {
+		return $http({
+				method: 'GET',
+				url: baseTenantUrl + '/authoring/v1/categories',
+				withCredentials: true
+			}).then(response => {
+				return response.data.items.find(item => {
+					return item.name === taxonomyName;
+				});
+			});
+	}
+	
+	/**
+	* Retrieves the categories under a given taxonomy id
+	* @return {String} id: The string id of the taxonomy
+	* @return {Promise} resolves to an Array of category Objects
+	*/
+	function getCategoriesByTaxonomyID(id) {
+		return $http({
+				method: 'GET',
+				url: baseTenantUrl + '/authoring/v1/categories/' + id + '/children',
+				withCredentials: true
+			}).then(response => {
+				return response.data.items;
+			});
+	}
+	
+	/**
+	* Retrieves the ID for the taxonomy with a given name
+	* @param {String} category: The string name of the taxonomy to fetch
+	* @return {Promise} resolves to an array of content item Objects
+	*/
+	function getContentItemsByCategoryName(categoryName) {
+		return $http({
+				method: 'GET',
+				url: baseTenantUrl + '/authoring/v1/search?q=*:*&wt=json&fq=type%3A%22Article%22&fq=classification:(content)&fl=id,document&fq=categories:(Article/' + categoryName + ')&sort=lastModified%20desc',
+				withCredentials: true
+			}).then(response => {
+				let contentItems = [];
+				response.data.documents.forEach(itemDoc => {
+					// the entire content item is available in the "document" field as a JSON string, so we'll parse it
+					let item = $.parseJSON(itemDoc.document).elements;console.log(item);
+					// normalize the values
+					item.id = itemDoc.id;
+					item.title = item.title ? item.title.value : 'untitled';
+					item.summary = item.summary ? item.summary.value : '';
+					item.author = item.author ? item.author.value : '';
+					item.body = item.body ? item.body.value : '';
+					item.imgSrc = item.image.asset ? baseTenantUrl + item.image.asset.resourceUri : '';
+					item.thumbnail = item.image.renditions && item.image.renditions.thumbnail ? baseTenantUrl + item.image.renditions.thumbnail.source : '';
+					let publishDate = item.publishDate ? new Date(item.publishDate.value) : new Date('');
+					item.publishDate = publishDate.toLocaleDateString();
+					// add the content item to the array
+					contentItems.push(item);
+				});
+				return contentItems;
+			});
+	}
+
+	return {
+		login,
+		getBaseURL,
+		getTaxonomyByName,
+		getCategoriesByTaxonomyID,
+		getContentItemsByCategoryName
+	};
+}]);
+
+
+// An angular component for a bootstrap tabbed navigation based on the Article taxonomy data
+module.component('taxonomyNavigation', {
+	controller: ['$rootScope', '$state', 'wchService', function ($rootScope, $state, wchService) {
+
+		// initialize data
+		this.tabs = [];
+		this.status = 'loading';	// set the status, one of 'loading', 'failed', 'done'
+
+		// load the navigation, first log in to Watson Content Hub
+		wchService.login().then(() => {
+			return wchService.getTaxonomyByName('Article');
+
+		// retrieve the Article taxonomy for this tenant
+		}).then(taxonomy => {
+			return wchService.getCategoriesByTaxonomyID(taxonomy.id);
+
+		// retrieve the categories under the Article taxonomy to populate the navigation
+		// go to the first navigation item
+		}).then(categories => {
+			this.tabs = categories;
+			this.status = 'done';
+			if(this.tabs.length) {
+				$state.transitionTo('cards', { category: this.tabs[0].name });
+			}
+
+		// print out any errors
+		}).catch(error => {
+			this.status = 'failed';
+			console.error('Error logging in and loading the navigation: %o', error);
+			// an empty category indicates a failure to the 'articleCards' component
+			$state.transitionTo('cards', { category: '' });
+		});
+
+
+		// update the navigation selection on state change
+		$rootScope.$on('$stateChangeSuccess', (event, toState, toParams) => {
+			const curCategory = toParams.category;
+			this.tabs.forEach(item => {
+				item.active = item.name === curCategory ? true : false;
+			});
+		});
+	}],
+
+	template: `
+		<span ng-if="$ctrl.status === 'loading'" class="pulse nav-item"><div></div><div></div><div></div><div></div><div></div><div></div></span>
+		<span ng-if="$ctrl.status === 'failed'" class="nav-item"><div class="alert" role="alert">Could not load navigation</div></span>
+		<ul ng-if="$ctrl.status === 'done'" class="nav nav-pills">
+			<li ng-repeat="tab in $ctrl.tabs track by tab.id" class="nav-item">
+				<a class="nav-link" ui-sref="cards({ category: tab.name })" ng-class="{ active: tab.active }" class="nav-link" href="#">{{tab.name}}</a>
+			</li>
+		</ul>
+	`
+});
+
+
+// An angular component for bootstrap cards based on the Article content items
+module.component('articleCards', {
+	controller: ['$stateParams', 'wchService', function ($stateParams, wchService) {
+
+		// initialize cards Array
+		this.cards = [];
+		// set the status, one of 'loading', 'failed', 'done'
+		this.status = 'loading';
+		// grab the current category name from the angular state parameters
+		this.categoryName = $stateParams.category;
+
+		// if a category name was passed in, load the content items tagged with it
+		if(this.categoryName === '') {
+			this.status = 'failed';
+		}
+		else if(this.categoryName) {
+			this.status = 'loading';
+			// load the Articles
+			wchService.getContentItemsByCategoryName(this.categoryName).then(items => {
+				this.cards = items;
+				this.status = 'done';
+
+			// print out any errors
+			}).catch(error => { 
+				this.status = 'failed';
+				console.error('Error loading the content items in the %o category: %o', this.categoryName, error);
+			});
+		} else {
+			this.status = 'loading';
+		}
+	}],
+
+	template: `
+		<div ng-if="$ctrl.status === 'loading'" class="spinner"></div>
+		<div ng-if="$ctrl.status === 'failed'" class="alert" role="alert">Could not load {{$ctrl.categoryName}} articles</div>
+		<div ng-if="$ctrl.status === 'done'" class="cards">
+			<a class="flex-card" ng-repeat="card in $ctrl.cards track by card.id" ui-sref="cards.details({ item: card, itemName: card.title })" href="#">
+				<span class="flex-card-header">
+					<img alt="{{card.title}}" src="{{card.thumbnail}}" />
+					<span class="card-title">
+						<h3>{{card.title}}</h3>
+					</span>
+				</span>
+				<span class="card-line"></span>
+				<span class="card-summary">
+					{{card.summary}}
+				</span>
+				<span class="card-meta">
+					by <span class="capital">{{card.author}}</span> on {{card.publishDate}}
+				</span>
+			</a>
+		</div>
+		<div ui-view></div>
+	`
+});
+
+
+// An angular component to show the entire Article in a modal dialog
+module.component('articleDetails', {
+	controller: ['$state', '$stateParams', function ($state, $stateParams) {
+
+		// grab the current Article from the angular state parameters
+		this.item = $stateParams.item;
+
+		// open the dialog
+		$('#detailsModal').modal('show');
+
+		// go back to the cards when the dialog closes
+		$('#detailsModal').on('hidden.bs.modal', function (e) {
+			$state.go('^');
+		});
+	}],
+
+	template: `
+		<div class="modal fade" id="detailsModal" tabindex="-1" role="dialog" aria-labelledby="detailsModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h4 class="modal-title" id="detailsModalLabel">{{$ctrl.item.title}}</h4>
+					</div>
+					<div class="modal-body">
+						<img src="{{$ctrl.item.imgSrc}}" class="img-fluid" alt="{{$ctrl.item.title}}">
+						<blockquote class="blockquote blockquote-reverse">
+							<p class="mb-0">{{$ctrl.item.summary}}</p>
+							<footer class="blockquote-footer">{{$ctrl.item.publishDate}} by <cite title="Source Title">{{$ctrl.item.author}}</cite></footer>
+						</blockquote>
+						<p>{{$ctrl.item.body}}</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" data-dismiss="modal">close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	`
+});
